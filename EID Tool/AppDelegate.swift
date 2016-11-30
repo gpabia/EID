83229c4e1e281992394b5363f79d05d1f4316e63
gpabia <gpabia19@gmail.com>
2016-11-30 11:32:22 +0100

    Initial Commit

---
+++ EID Tool/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  EID Tool
+//
+//  Created by Guillaume Pabia on 30/11/2016.
+//  Copyright © 2016 Boquet. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ EID Tool/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  EID Tool
+//
+//  Created by Guillaume Pabia on 30/11/2016.
+//  Copyright © 2016 Boquet. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
